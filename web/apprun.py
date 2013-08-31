8c1426b69f4ac6db5df4554cc38acadcdfd22f38
comger <comger@gmail.com>
2013-08-31 10:45:45 +0800

    .

--- web/apprun.py
+++ web/apprun.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*- 
+# -*- coding:utf-8 -*-
 """
     author comger@gmail.com
 """
 import sys
-from kpages.app import run
+import json
+
+from datetime import datetime
+from pprint import pprint
+from kpages import run
+
+
+def callback(app):
+    '''
+        print app config
+    '''
+
+    print "Start time: {0}".format(datetime.now().isoformat(" "))
+    print "Config Params"
+    for k in sorted(app.settings.keys()):
+        if k.startswith("__"):
+            continue
+        print "  {0:<20} : {1}".format(k, app.settings[k])
+
+    print
+    print "Router Handlers"
+    pprint(app.handlers)
 
 if __name__ == "__main__":
-    run(*sys.argv)
+    try:
+        run(callback)
+    except KeyboardInterrupt:
+        print 'exit app '
+
+# vim: ts=4 sw=4 sts=4 expandtab
