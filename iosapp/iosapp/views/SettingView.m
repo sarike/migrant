bcf2338c484975549e27e278badd8c0a540a709b
comger <comger@gmail.com>
2013-09-03 18:30:53 +0800

    .

--- iosapp/iosapp/views/SettingView.m
+++ iosapp/iosapp/views/SettingView.m
@@ -32,28 +32,28 @@
     [super viewDidLoad];
 
     self.settingsInSection = [[NSMutableDictionary alloc] initWithCapacity:3];
-    BOOL isLogin = YES;
+    isLogin = [[LocalConfig Instance] shareconfig:@"uid"]?YES:NO;
 
     NSArray *first = [[NSArray alloc] initWithObjects:
-                      [[SettingModel alloc] initWith:isLogin ? @"我的资料 (收藏/关注/粉丝)":@"登录" andImg:@"account" andTag:1 andTitle2:nil],
+                      [[SettingModel alloc] initWith:isLogin ? @"我的资料 ":@"登录" andImg:@"account" andTag:1 andTitle2:nil],
                       [[SettingModel alloc] initWith: @"注销" andImg:@"exit" andTag:2 andTitle2:nil],
                       nil];
     NSArray *second = [[NSArray alloc] initWithObjects:
-                       [[SettingModel alloc] initWith:@"软件" andImg:@"software" andTag:3 andTitle2:nil],
-                       [[SettingModel alloc] initWith:@"搜索" andImg:@"search" andTag:4 andTitle2:nil],
+                       [[SettingModel alloc] initWith:@"项目" andImg:@"software" andTag:3 andTitle2:nil],
+                       [[SettingModel alloc] initWith:@"职位" andImg:@"software" andTag:4 andTitle2:nil],
+                       [[SettingModel alloc] initWith:@"搜索" andImg:@"search" andTag:5 andTitle2:nil],
                        nil];
     NSArray *third = [[NSArray alloc] initWithObjects:
-                      [[SettingModel alloc] initWith:@"意见反馈" andImg:@"feedback" andTag:5 andTitle2:nil],
-                      [[SettingModel alloc] initWith:@"官方微博" andImg:@"weibo" andTag:6 andTitle2:nil], 
+                      [[SettingModel alloc] initWith:@"意见反馈" andImg:@"feedback" andTag:6 andTitle2:nil],
                       [[SettingModel alloc] initWith:@"关于我们" andImg:@"logo" andTag:7 andTitle2:nil],
                       [[SettingModel alloc] initWith:@"检测更新" andImg:@"setting" andTag:8 andTitle2:nil],
-                      [[SettingModel alloc] initWith:@"给我评分" andImg:@"rating" andTag:9 andTitle2:nil],
                       nil];
     [self.settingsInSection setObject:first forKey:@"帐号"];
     [self.settingsInSection setObject:second forKey:@"反馈"];
     [self.settingsInSection setObject:third forKey:@"关于"];
     self.settings = [[NSArray alloc] initWithObjects:@"帐号",@"反馈",@"关于",nil];
 }
+
 - (void)viewDidAppear:(BOOL)animated
 {
     //[[NSNotificationCenter defaultCenter] postNotificationName:@"login" object:[Config Instance].isCookie ? @"1" : @"0"];
@@ -68,7 +68,7 @@
 {
     NSArray *first = [self.settingsInSection objectForKey:@"帐号"];
     SettingModel *firstLogin = [first objectAtIndex:0];
-    firstLogin.title = [[LocalConfig Instance] shareconfig:@"uid"] ? @"我的资料 (收藏/关注/粉丝)" : @"登录";
+    firstLogin.title = [[LocalConfig Instance] shareconfig:@"uid"] ? @"我的资料" : @"登录";
     [self.tableSettings reloadData];
 }
 #pragma TableView的处理
@@ -81,9 +81,21 @@
     SettingModel *action = [sets objectAtIndex:[indexPath row]];
     switch (action.tag) {
         case 1:{
-            LoginView *login = [[LoginView alloc] init];
-            login.hidesBottomBarWhenPushed = YES;
-            [self.navigationController pushViewController:login animated:YES];
+            if(!isLogin){
+                LoginView *login = [[LoginView alloc] init];
+                login.hidesBottomBarWhenPushed = YES;
+                [self.navigationController pushViewController:login animated:YES];
+            }
+
+        }
+            break;
+
+        case 2:
+        {
+            
+            [self refresh];
+            
+            
         }
             break;
         default:
